83ecec36e9f2fd383a5304f104f7dc71c3e6d393
Lucy Toman <lucy@toman.me.uk>
2022-05-10 15:17:46 +0200

    plumbs prisma into frontend

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
-import Head from "next/head";
-import Image from "next/image";
+// import Head from "next/head";
+// import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import prisma from "lib/prisma";
 
-export default function Home() {
+export default function Home({ questions }) {
     return (
         <div className={styles.container}>
             <h1>Setting up Next and Prisma</h1>
+            <ul>
+                {questions.map((question, index) => {
+                    return (
+                        <li key={index}>
+                            {question.first} {question.answer} {question.second}
+                        </li>
+                    );
+                })}
+            </ul>
         </div>
     );
+}
+
+export async function getServerSideProps() {
+    let questions = await prisma.question.findMany();
+
+    return {
+        props: {
+            questions,
+        },
+    };
 }
